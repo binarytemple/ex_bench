e7ec67cbd137ee45f2a25956cf9f2eab71fec8cf
bryan <bryan.hunt@erlang-solutions.com>
2019-07-13 12:18:48 +0100

    every config level gets content( fix elixir 1.6)

--- config/dev.exs
+++ config/dev.exs
@@ -1,10 +1,6 @@
 use Mix.Config
 
-# config :logger,
-#  backends: [:console],
-#    compile_time_purge_matching: [
-#          [level_lower_than: :info]
-#        ]
+
 config :logger,
   level: :debug
 
--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,2 @@
-
+config :logger,
+  level: :debug

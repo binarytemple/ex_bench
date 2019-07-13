32f3967c2585716e52dd962a509b3c9d9c5a6dbb
bryan <bryan.hunt@erlang-solutions.com>
2019-07-13 12:19:15 +0100

    lets see if it runs on 1.5

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,5 @@
-
 config :logger,
- backends: [:console],
-   compile_time_purge_matching: [
-         [level_lower_than: :info]
-       ]
+  backends: [:console],
+  compile_time_purge_matching: [
+    [level_lower_than: :info]
+  ]

2d0a73c0a31ffaf4784bfc60a73a8f2d875ffa5e
bryan <bryan.hunt@erlang-solutions.com>
2019-07-29 17:40:51 +0100

    bump to 0.2.13 for tag/publish

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBench.MixProject do
   def project do
     [
       app: :ex_bench,
-      version: "0.2.12",
+      version: "0.2.13",
       elixir: "~> 1.6 or ~> 1.7 or ~> 1.8 or ~> 1.9",
       start_permanent: false,
       package: package(),

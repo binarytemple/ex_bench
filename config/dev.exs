8f85105281cb12ac71a1e13a228ecc78e6601de3
bryan <bryan.hunt@erlang-solutions.com>
2019-07-30 09:09:03 +0100

    first attempt

--- config/dev.exs
+++ config/dev.exs
@@ -6,11 +6,3 @@ config :logger,
 # ,
 # handle_otp_reports: true,
 # handle_sasl_reports: true
-
-config :ex_bench,
-  workers: 10,
-  overflow: 2,
-  concurrency: 3,
-  bench_fun: fn x -> IO.inspect(x) end,
-  producer: ExBench.FileProducer,
-  producer_argument: %{filename: "priv/example.consult"}
